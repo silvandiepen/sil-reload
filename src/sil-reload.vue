57ec14df9bb5663c5eaf1c2e9deade2e30294968
Sil van Diepen <me@silvandiepen.com>
2018-10-09 20:18:39 +0200

    add the reloading logic

--- src/sil-reload.vue
+++ src/sil-reload.vue
@@ -1,35 +1,123 @@
+<template>
+	<div class="reload" :class="{'reload--active' : scrolling, 'reload--reload' : reloading}"></div>
+</template>
+
 <script>
 export default {
-	name: 'silReload', // vue component name
 	data() {
 		return {
-			counter: 5,
-			initCounter: 5
+			timeoutTimer: function() {},
+			timoutReload: function() {},
+			reloading: false,
+			scrolling: false
 		};
+	},
+	mounted() {
+		let _this = this;
+		if (process.browser) {
+			window.addEventListener('scroll', function(e) {
+				let triggerPoint = 0;
+				if (window.innerWidth < 750) {
+					triggerPoint = -100;
+				} else {
+					triggerPoint = -50;
+				}
+				if (window.scrollY < triggerPoint) {
+					_this.scrolling = true;
+					timeoutTimer = setTimeout(() => {
+						_this.reloading = true;
+						document.querySelector('.page').classList.add('fadeOut');
+						timoutReload = setTimeout(() => {
+							location.reload();
+						}, 500);
+					}, 500);
+				} else {
+					_this.scrolling = false;
+					clearTimeout(_this.timeoutTimer);
+					clearTimeout(_this.timeoutReload);
+				}
+			});
+		}
 	}
 };
 </script>
 
-<template>
-    <div class="silReload">
-        <p>The counter is set to <b>{{counter}}</b>.</p>
-        <button @click="counter += 1">Click +1</button>
-        <button @click="counter -= 1">Click -1</button>
-        <button @click="counter = initCounter">Reset</button>
-    </div>
-</template>
-
 <style scoped>
-.silReload {
-	display: block;
-	width: 400px;
-	margin: 25px auto;
-	border: 1px solid #ccc;
-	background: #eaeaea;
-	text-align: center;
-	padding: 25px;
+.page {
+	filter: blur(0px);
+	transform: scale(1);
+	opacity: 1;
+	transition: filter 0.4s, opacity 0.4s, transform 0.4s;
 }
-.silReload p {
-	margin: 0 0 1em;
+.page.fadeOut {
+	opacity: 0.5;
+	transform: scale(1.1);
+	filter: blur(20px);
+}
+.reload {
+	width: 100px;
+	height: 100px;
+	position: fixed;
+	left: 50%;
+	top: 0;
+	transform: translateX(-50%);
+}
+@media screen and (min-width: 1280px) {
+	.reload {
+		width: 50px;
+		height: 50px;
+		margin-top: 10px;
+	}
+}
+
+.reload:before {
+	content: '';
+	width: 50%;
+	height: 50%;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%) scale(0) rotate(0deg);
+	display: block;
+	transition: transform 0.3s;
+	border: 2px solid #7f7f7f;
+	border-radius: 50%;
+	border-bottom: 2px solid pink;
+	border-top: 2px solid lightblue;
+	border-left: 2px solid lightgreen;
+}
+@media screen and (min-width: 1280px) {
+	.reload:before {
+		width: 100%;
+		height: 100%;
+	}
+}
+
+.reload--active:before {
+	transform: translate(-50%, -50%) scale(1) rotate(0deg);
+	animation: rotating 1s 0.3s linear infinite;
+}
+
+.reload--reload:before {
+	animation: reload 0.3s ease-in-out once;
+}
+
+@keyframes rotating {
+	from {
+		transform: translate(-50%, -50%) scale(1) rotate(0deg);
+	}
+	to {
+		transform: translate(-50%, -50%) scale(1) rotate(360deg);
+	}
+}
+@keyframes reload {
+	from {
+		opacity: 1;
+		transform: translate(-50%, -50%) scale(1) rotate(0deg);
+	}
+	to {
+		opacity: 0;
+		transform: translate(-50%, -50%) scale(2) rotate(0deg);
+	}
 }
 </style>
